3a28354dd99c84a59b3715c6188bd9547d501a57
QilongPan <3102377627@qq.com>
2018-07-17 20:12:18 +0800

    update insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# @Author: Qilong Pan
+# @Date:   2018-07-17 19:21:04
+# @Last Modified by:   Qilong Pan
+# @Last Modified time: 2018-07-17 20:08:08
+
+#insert sort
+
+def insertion_sort(temp):
+
+    if len(temp) <= 1:
+    	pass
+    else:
+    	for i in range(1,len(temp)):
+			key = temp[i]
+			index = i
+			for j in range(i-1,-1,-1):
+				if key < temp[j]:
+					temp[j+1] = temp[j]
+					index = j
+			temp[index] = key
+
+
+		
+
+#main function
+if __name__ == "__main__":
+	temp = [4,1,3,7,6,2]
+	insertion_sort(temp)
+	print(temp)
+
+'''
+for i in range(1,len(temp)):
+			key = temp[i]
+			index = i
+			for j in range(i-1,-1,-1):
+				if key < sortedList[j]:
+					temp[j+1] = temp[j]
+					index = j
+			temp[index] = key
+'''
--- test.py
+++ test.py
@@ -1,6 +1,2 @@
-input_vecs = [[1,1], [0,0], [1,0], [0,1]]
-labels = [1, 0, 0, 0]
-samples = zip(input_vecs, labels)
-print(input_vecs)
-print(labels)
-print(samples)
+for i in range(10,-1,-1):
+	print(i)
