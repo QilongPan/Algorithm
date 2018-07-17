f29f9d006a4df2e59635f5c30867a7dedb6578ab
QilongPan <3102377627@qq.com>
2018-07-17 20:21:25 +0800

    update

--- insertion_sort.py
+++ insertion_sort.py
@@ -2,12 +2,10 @@
 # @Author: Qilong Pan
 # @Date:   2018-07-17 19:21:04
 # @Last Modified by:   Qilong Pan
-# @Last Modified time: 2018-07-17 20:08:08
+# @Last Modified time: 2018-07-17 20:16:36
 
 #insert sort
-
 def insertion_sort(temp):
-
     if len(temp) <= 1:
     	pass
     else:
@@ -20,22 +18,8 @@ def insertion_sort(temp):
 					index = j
 			temp[index] = key
 
-
-		
-
 #main function
 if __name__ == "__main__":
 	temp = [4,1,3,7,6,2]
 	insertion_sort(temp)
 	print(temp)
-
-'''
-for i in range(1,len(temp)):
-			key = temp[i]
-			index = i
-			for j in range(i-1,-1,-1):
-				if key < sortedList[j]:
-					temp[j+1] = temp[j]
-					index = j
-			temp[index] = key
-'''
